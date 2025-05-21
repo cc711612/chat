d4e91a90888dcfb3e01ed5995d85c367bc4fb439
Roy <cc711612@gmail.com>
2025-05-21 21:16:11 +0800

    調整 cros

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,19 +7,24 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('server.port') ?? 3001;
-  
+
   // 從環境配置中獲取 CORS 設定
   const corsEnabled = configService.get('cors.enabled');
   console.log('CORS_ENABLED value from configService:', corsEnabled);
   if (corsEnabled) {
+    let origin = configService.get('cors.origin');
+    // 若設為 * 則全開
+    if (origin === '*' || origin === true) {
+      origin = true;
+    }
     app.enableCors({
-      origin: configService.get('cors.origin'),
+      origin,
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
       credentials: configService.get('cors.credentials'),
     });
-    console.log(`CORS enabled with origin: ${configService.get('cors.origin')}`);
+    console.log(`CORS enabled with origin: ${origin}`);
   }
-  
+
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
 }
